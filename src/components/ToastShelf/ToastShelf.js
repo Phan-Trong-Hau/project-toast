6f0d4ab520b6db17fc2033f09e6d9237445ecc2b
PhanTrongHau <haupk2003@gmail.com>
2024-07-23 00:33:23 +0700

    add action for toast

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import Toast from '../Toast';
-import styles from './ToastShelf.module.css';
+import Toast from "../Toast";
+import styles from "./ToastShelf.module.css";
 
-function ToastShelf() {
+function ToastShelf({ toasts, handleRemoveItem }) {
+
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+      {toasts.map((toast) => (
+        <li className={styles.toastWrapper} key={toast.id}>
+          <Toast
+            variant={toast.variant}
+            handleRemoveItem={() => handleRemoveItem(toast.id)}
+          >
+            {toast.message}
+          </Toast>
+        </li>
+      ))}
     </ol>
   );
 }
 
-export default ToastShelf;
+export default React.memo(ToastShelf);
